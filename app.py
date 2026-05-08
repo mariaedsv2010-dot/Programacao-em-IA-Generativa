09d2a7e66deb362964273803c0864556b502390b
mariaedsv2010-dot <mariaedsv2010@gmail.com>
2026-05-08 17:54:55 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+# CRIE SUA APLICAÇÃO, COMO SE FOSSE O SEU PORTFÓLIO PESSOAL.
+# COLOQUE NOME
+# CURIOSIDADES
+# UMA IMAGEM QUE TE DEFINE
+#  O SEU LINK DO GITHUB
+
+
+import streamlit as st 
+st.title('MEU PORTOFÓLIO')
+ 
+st.text('NOME:')
+st.caption('Maria Eduarda dos Santos Vicente')
+
+st.text('CURIOSIDADES:')
+st.caption('Eu gosto estudar programação, ir a praia, me maquiar,sair com a minha familia, viajar, conhecer coisas novas, eu adoro tirar fotos ')
+
+st.image('download.jpg')
+
+st.text('MEU GITHUB')
+st.write('[https://github.com/mariaedsv2010-dot](https://github.com/mariaedsv2010-dot)')
