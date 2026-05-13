0401bdb10590e85aa0be00410776a32fd1164d1d
mariaedsv2010-dot <mariaedsv2010@gmail.com>
2026-05-13 16:41:13 -0300

    Add files via upload

---
+++ aula4/app.py
@@ -0,0 +1,65 @@
+# # import streamlit as st
+# import minha_funcao as ms
+
+
+# import streamlit as st
+
+
+# st.title('titulo')
+
+
+# ms.soma(10,20)
+# ms.soma(200,500)
+
+
+# estrutura de dados
+# espaço na memória da máquina
+
+
+# variavel   =  10
+# lista  =  [1,2,3]
+# dicionario =  {'a':10, 'b':20}
+# conjuntos =  {10,20,303,30}
+# tupla = (10,20,30,30)
+
+
+
+# estutra de fluxo de controle 
+# palavra_reserva condição
+
+
+# if 10 >2:
+#     print('é maior')
+
+
+# for i in range(10):
+#     print(i)
+
+
+# while 10 < 1:
+#     print('faça')    
+
+
+# try :
+
+
+# funções 
+# palavras ()
+# cria -  def nome ():
+# Ação 
+# função são nativas da lingugame 
+
+
+# print()
+# len()
+# input()
+# sum()
+# ....
+
+
+
+# def nome():
+#     print('teste')
+
+
+# nome()
